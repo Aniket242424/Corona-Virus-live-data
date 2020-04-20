4b0a7b6984dbf40c93ba5de061e79febf42b8735
Aniket242424 <amahangade24@gmail.com>
2020-04-20 18:37:25 +0530

    Updates

---
+++ precache-manifest.f7ed6e8f08cf8fecf69edd6c4593cceb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea8f5a282bb9be9ce828b84f38183a6",
+    "url": "/Corona-Virus-Live-Data/index.html"
+  },
+  {
+    "revision": "eee1e383abace5b96c19",
+    "url": "/Corona-Virus-Live-Data/static/css/2.66165e39.chunk.css"
+  },
+  {
+    "revision": "de5cd177de7b65d96572",
+    "url": "/Corona-Virus-Live-Data/static/css/main.dd831c1d.chunk.css"
+  },
+  {
+    "revision": "eee1e383abace5b96c19",
+    "url": "/Corona-Virus-Live-Data/static/js/2.2eff74f7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-Virus-Live-Data/static/js/2.2eff74f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5cd177de7b65d96572",
+    "url": "/Corona-Virus-Live-Data/static/js/main.84e23ca3.chunk.js"
+  },
+  {
+    "revision": "a9ae8afb9ab8173adc86",
+    "url": "/Corona-Virus-Live-Data/static/js/runtime-main.fcce2aef.js"
+  },
+  {
+    "revision": "a7dc73e2bc2a4d635326aecc90df7dfe",
+    "url": "/Corona-Virus-Live-Data/static/media/Covid-19.a7dc73e2.png"
+  }
+]);
